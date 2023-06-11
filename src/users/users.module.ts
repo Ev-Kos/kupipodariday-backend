a6303681ab00b4baa8fbb684b4973b81461345c6
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-06-11 13:14:14 +0300

    added controller, module, service for users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Wish } from '../wishes/entities/wish.entity';
+import { UsersService } from './users.service';
+import { AuthService } from 'src/auth/auth.service';
+import { WishesModule } from 'src/wishes/wishes.module';
+import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Wish])],
-  exports: [],
-  providers: [],
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([User, AuthService]), WishesModule],
+  exports: [UsersService],
+  providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashService } from 'src/auth/hash/hash.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/CreateUserDto';
+import { UpdateUserDto } from './dto/UpdateUserDto';
+
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private hashService: HashService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    user.password = await this.hashService.generateHash(user.password);
+    return await this.usersRepository.save(user);
+  }
+
+  async findOne(id: number): Promise<User> {
+    return await this.usersRepository.findOneBy({ id });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findMany(user): Promise<User[]> {
+    return await this.usersRepository.find({
+      where: [{ username: user.query }, { email: user.query }],
+    });
+  }
+
+  async updateOne(id: number, updateUserDto: UpdateUserDto) {
+    let password = updateUserDto.password;
+    if (password) {
+      password = await this.hashService.generateHash(password);
+    }
+    await this.usersRepository.update({ id }, updateUserDto);
+    return await this.findOne(id);
+  }
+
+  async findByUsername(username: string) {
+    return await this.usersRepository.findOne({
+      where: {
+        username: username,
+      },
+      relations: {
+        wishes: true,
+        wishlists: true,
+      },
+    });
+  }
+
+  async removeOne(id: number): Promise<void> {
+    await this.usersRepository.delete({ id });
+  }
 }
